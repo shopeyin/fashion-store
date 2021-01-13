14e32af3aceae8969542eac2397ce66a7e5def82
oladimeji <tommyslipz@gmail.com>
2021-01-13 16:40:25 +0100

    f

---
+++ sendgrid/SendGrid.js
@@ -0,0 +1,9 @@
+// echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+// echo "sendgrid.env" >> .gitignore
+// source ./sendgrid.env
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+// 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
+
+
--- server.js
+++ server.js
@@ -35,17 +35,68 @@ app.get("/service-worker.js", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
-app.post("/payment", (req, res) => {
-  const body = {
-    source: req.body.token.id,
-    amoount: req.body.amoount,
-    currency: "usd",
-  };
-  stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
-    } else {
-      res.status(200).send({ success: stripeRes });
-    }
-  });
+// app.post("/payment", (req, res) => {
+//   const body = {
+//     source: req.body.token.id,
+//     amoount: req.body.amoount,
+//     currency: "usd",
+//   };
+//   stripe.charges.create(body, (stripeErr, stripeRes) => {
+//     if (stripeErr) {
+//       res.status(500).send({ error: stripeErr });
+//     } else {
+//       res.status(200).send({ success: stripeRes });
+//     }
+//   });
+// });
+
+
+const sgMail = require('@sendgrid/mail')
+
+const API_KEY = "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"
+sgMail.setApiKey(API_KEY)
+
+
+app.post("/cart", (req, res) => {
+  const msg = {
+    to: 'tommyslipz@gmail.com', // Change to your recipient
+    from: 'oladimejicoder@gmail.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    // text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>how are you even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
 });
+
+
+
+
+
+
+// function SendMail() {
+//   const msg = {
+//     to: 'tommyslipz@gmail.com', // Change to your recipient
+//     from: 'oladimejicoder@gmail.com', // Change to your verified sender
+//     subject: 'Sending with SendGrid is Fun',
+//     // text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>how are you even with Node.js</strong>',
+//   }
+//   sgMail
+//     .send(msg)
+//     .then(() => {
+//       console.log('Email sent')
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//     })
+// }
+ 
+// SendMail()
+
