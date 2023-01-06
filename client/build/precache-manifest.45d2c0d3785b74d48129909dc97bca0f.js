5462a1fe7d303f858e17a9b1a4094223a2ea24d0
oladimeji <tommyslipz@gmail.com>
2023-01-06 05:56:15 +0000

    added ci/cd

---
+++ client/build/precache-manifest.45d2c0d3785b74d48129909dc97bca0f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715039f8d58628a119ad7fdfa897ec1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c766a88c98eba03a2d4d",
+    "url": "/static/css/4.06f64afe.chunk.css"
+  },
+  {
+    "revision": "417629bdf2ec595c68dc",
+    "url": "/static/css/5.90420e11.chunk.css"
+  },
+  {
+    "revision": "0881085eb4a11cbc8dea",
+    "url": "/static/css/6.76d2c869.chunk.css"
+  },
+  {
+    "revision": "b18b292309a29b2e21d0",
+    "url": "/static/css/7.d73497d3.chunk.css"
+  },
+  {
+    "revision": "08e6882fba007003798f",
+    "url": "/static/css/8.6a1eccb2.chunk.css"
+  },
+  {
+    "revision": "5155a23191414acdd36d",
+    "url": "/static/css/9.905697fb.chunk.css"
+  },
+  {
+    "revision": "5ef3057bc2aab259d130",
+    "url": "/static/css/main.8499af48.chunk.css"
+  },
+  {
+    "revision": "ee596130f83b91bb9629",
+    "url": "/static/js/10.27d1bbc3.chunk.js"
+  },
+  {
+    "revision": "6c5b0fda791b2b2c4d9a",
+    "url": "/static/js/2.ba6d70f5.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.ba6d70f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7531ceab6769dcc7c2e",
+    "url": "/static/js/3.a27fa2e1.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.a27fa2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c766a88c98eba03a2d4d",
+    "url": "/static/js/4.245f47d1.chunk.js"
+  },
+  {
+    "revision": "417629bdf2ec595c68dc",
+    "url": "/static/js/5.2a373718.chunk.js"
+  },
+  {
+    "revision": "0881085eb4a11cbc8dea",
+    "url": "/static/js/6.7b734481.chunk.js"
+  },
+  {
+    "revision": "b18b292309a29b2e21d0",
+    "url": "/static/js/7.ec68e962.chunk.js"
+  },
+  {
+    "revision": "08e6882fba007003798f",
+    "url": "/static/js/8.e159e77e.chunk.js"
+  },
+  {
+    "revision": "5155a23191414acdd36d",
+    "url": "/static/js/9.48b10fe6.chunk.js"
+  },
+  {
+    "revision": "5ef3057bc2aab259d130",
+    "url": "/static/js/main.cd80ca17.chunk.js"
+  },
+  {
+    "revision": "042a7654bbadabb7cb05",
+    "url": "/static/js/runtime-main.c97de4b8.js"
+  },
+  {
+    "revision": "22fdf1f26e69792fa77ced7e372c3baf",
+    "url": "/static/media/photo-8.22fdf1f2.jpg"
+  },
+  {
+    "revision": "ce8b4b181c10bd285af1b522532ac189",
+    "url": "/static/media/photo2.ce8b4b18.jpg"
+  },
+  {
+    "revision": "e1d1eb86482d83bfbaf22ca4fd97ca3e",
+    "url": "/static/media/photo3.e1d1eb86.jpg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
